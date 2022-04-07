577c69d2c1600345dfe0dbfdf4e460432b6eb571
“Allen <allenalejandro3@gmail.com>
2022-04-07 12:20:10 -0500

    creates starter files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # weather-app
+
+## task
+
+1. Setup a new public repo on GitHub
+2. Create a new react app
+3. Add mui https://mui.com/ for styling and components
+4. Add recoil https://recoiljs.org/ for state management
+5. Add react-query https://react-query.tanstack.com/ for xhr
+6. Create a component with a text box that accepts a zip code
+7. Take that zip code and figure out how to generate a call to the appropriate api on https://developer.accuweather.com/
+8. Display the data returned as a 7 day forecast
+9. Push all code to GitHub and share the link with me.
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+    compress: true,
+    port: 3000,
+  },
+};
+
+module.exports = config;
